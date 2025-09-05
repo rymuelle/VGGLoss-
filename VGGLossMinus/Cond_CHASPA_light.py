595f5120dd48cbdc22cd3f5f18577837482fb0a5
Ryan Mueller <ryan.dalrymple.mueller@gmail.com>
2025-09-04 21:19:29 -0400

    Back to LayerNorm

--- VGGLossMinus/Cond_CHASPA_light.py
+++ VGGLossMinus/Cond_CHASPA_light.py
@@ -190,8 +190,8 @@ class CHASPABlock(nn.Module):
 
         # self.grn = GRN(ffn_channel // 2)
 
-        self.norm1 = nn.GroupNorm(1, c) #nn.GroupNorm(1, c) #LayerNorm2d(c)
-        self.norm2 = nn.GroupNorm(1, c) #LayerNorm2d(c)
+        self.norm1 = LayerNorm2d(c)
+        self.norm2 = LayerNorm2d(c)
 
         self.dropout1 = (
             nn.Dropout(drop_out_rate) if drop_out_rate > 0.0 else nn.Identity()
