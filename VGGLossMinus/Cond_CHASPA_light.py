8d4cb5fa451becd9da3f6d3b0e199d0f728ec77e
Ryan Mueller <ryan.dalrymple.mueller@gmail.com>
2025-09-06 23:13:58 -0400

    Playing with adding an NKA to the encoder

--- VGGLossMinus/Cond_CHASPA_light.py
+++ VGGLossMinus/Cond_CHASPA_light.py
@@ -281,11 +281,13 @@ class GELUWrapper(nn.Module):
         super().__init__()
         self.activation = nn.GELU()
         self.beta = nn.Parameter(torch.zeros((1, channels, 1, 1)), requires_grad=True)
+        self.NKA = NKA(channels)
 
     def forward(self, input):
         inp = input[0]
         cond = input[1]
-        x = self.activation(inp)
+        x = self.NKA(inp)
+        x = self.activation(x)
         return (inp + self.beta * x, cond)
 
 class CHASPA_light(nn.Module):
