1fb6a1b288472cf38a3278510ea79f5390120d71
Ryan Mueller <ryan.dalrymple.mueller@gmail.com>
2025-09-07 13:22:08 -0400

    Maybe it doesn't need to be an attention mechanism

--- VGGLossMinus/Cond_CHASPA_light_CCA.py
+++ VGGLossMinus/Cond_CHASPA_light_CCA.py
@@ -153,7 +153,7 @@ class NKA(nn.Module):
         # Non-linearity
         attn = self.sg(attn)
         # Apply attention map
-        out = x * self.attention(attn)
+        out = self.attention(attn)
         return out
 
 class LKA(nn.Module):
